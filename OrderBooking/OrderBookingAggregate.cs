64f6821302fdd1ad822e68727dde9e42190a7a28
Sarwan Surchi <sarwan.h.najim@gmail.com>
2024-08-24 03:42:38 +0300

    Add orderbooking and orderbooking aggregate

---
+++ OrderBooking/OrderBookingAggregate.cs
@@ -0,0 +1,8 @@
+﻿using AsyncHandler.EventSourcing.AggregateRoot;
+
+namespace OrderBooking;
+
+public class OrderBookingAggregate(int orderId) : AggregateRoot(orderId)
+{
+    public OrderStatus Status { get; private set; }
+}
